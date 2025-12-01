4cfe5f9e998d6b014d3e3a9dd30b5a10c0f07da5
fenix <fenix@librebits.info>
2025-12-01 13:52:15 +0100

    ++ generar-datos-de-alumnos-aleatoriamente/interaccion-con-mysql.py

--- generar-datos-de-alumnos-aleatoriamente/interaccion-con-mysql.py
+++ generar-datos-de-alumnos-aleatoriamente/interaccion-con-mysql.py
@@ -1,26 +1,3 @@
-# ¿ un Problema para las Pythonisas ?
-
-Bien, en nuestra nueva aventura de programación, en nuestro nuevo proyecto, Nos piden que generemos una gran cantidad de datos de alumnos ficticios para alimentar la base de datos SQL mediante las operaciones  'insert'  correspondientes.
-
-
-## Fases de la solución  del problema
-
-Generar datos personales de alumnos ficticios. Con su 
-- nombre 
-- apellidos, 
-- correo electrónico
-
-de les estudiantes de la asignatura en cuestión.
-  
-
-### Datos, Listas y Diccionarios : 
-#### ¿Como organizar grandes cantidades de datos?
-
-Juguemos con estructuras de datos como lista y diccionarios para hallar la solución.
-
-#### Veamos como realizar los inserts a la Base de Datos SQL
-
-```python
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -57,4 +34,3 @@ query = "INSERT INTO b VALUES (1, 2)"
 run_query(query)
 query2 = "INSERT INTO b (b1, b2) VALUES ('%s', '%s')" % (dato1, dato2)
 run_query(query2)
-```
